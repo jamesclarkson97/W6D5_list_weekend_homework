3b43de05d69816e425dbc51b6e37350adc032274
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-24 15:46:44 +0100

    no error upon pressing submit. But no list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('#button');
-    button.addEventListener('click', handleButtonClick);
+    // const button = document.querySelector('#button');
+    // button.addEventListener('click', handleButtonClick);
   
-    const textInput = document.querySelector('#input');
-    textInput.addEventListener('input', handleInput);
+    // const textInput = document.querySelector('#input');
+    // textInput.addEventListener('input', handleInput);
   
-    const select = document.querySelector('#select');
-    select.addEventListener('change', handleSelectChange);
+    // const select = document.querySelector('#select');
+    // select.addEventListener('change', handleSelectChange);
   
     const form = document.querySelector('#form');
     form.addEventListener('submit', handleFormSubmit);
   });
+
+
+
+//   const handleButtonClick = function () {
+//     const resultParagraph = document.querySelector('#button-result');
+//     resultParagraph.textContent = 'That button has definitely been clicked.';
+//   };
+  
+//   const handleInput = function (event) {
+//     const resultParagraph = document.querySelector('#input-result');
+//     resultParagraph.textContent = `You typed: ${event.target.value}`;
+//   };
+  
+//   const handleSelectChange = function (event) {
+//     const resultParagraph = document.querySelector('#select-result');
+//     resultParagraph.textContent = `You went with: "${event.target.value}".`;
+//   };
+  
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+    const resultParagraph = document.createElement('li');
+    resultParagraph.textContent = `${event.target.name.value} is a 
+      ${event.target.category.value} anaemia.`;
+    
+    const list = document.querySelector('ul');
+    list.appendChild(resultParagraph);
+  };
+
+    
