cc463fb64e0a7159d5528ec164426c3f3119c7ee
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-27 09:31:26 +0100

    Changed up some css styling

--- js/app.js
+++ js/app.js
@@ -8,16 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteAllButton = document.querySelector('#delete_all');
     deleteAllButton.addEventListener('click', handleAllButtonClick);
   
-    // const textInput = document.querySelector('#input');
-    // textInput.addEventListener('input', handleInput);
-  
-    // const select = document.querySelector('#select');
-    // select.addEventListener('change', handleSelectChange);
-  
     const form = document.querySelector('#form');
     form.addEventListener('submit', handleFormSubmit);
   });
 
+  
   const handleFirstButtonClick = function () {
     const result = document.querySelector('li');
     result.remove();
@@ -34,23 +29,13 @@ document.addEventListener('DOMContentLoaded', () => {
         result.removeChild(result.lastChild);
       };
   };
-//   const handleInput = function (event) {
-//     const resultParagraph = document.querySelector('#input-result');
-//     resultParagraph.textContent = `You typed: ${event.target.value}`;
-//   };
-  
-//   const handleSelectChange = function (event) {
-//     const resultParagraph = document.querySelector('#select-result');
-//     resultParagraph.textContent = `You went with: "${event.target.value}".`;
-//   };
   
   const handleFormSubmit = function (event) {
     event.preventDefault();
     const resultParagraph = document.createElement('li');
     resultParagraph.textContent = `${event.target.name.value} is a cause of
       ${event.target.category.value} anaemia.`;
-    // resultParagraph.id = `${event.target.name.value} ${event.target.category.value}`
-
+    
     const list = document.querySelector('#causes');
     list.appendChild(resultParagraph);
   };
